2c9bdd3b6999def0781a1dd6804255f50c32461c
Vadim Polovinko <vadimpolovinko89@gmail.com>
2024-07-30 01:03:28 +0300

    feat: add some logic before tests

--- src/index.js
+++ src/index.js
@@ -34,18 +34,35 @@ module.exports = function toReadable(number) {
 
     let resultString = "";
 
-    function makeMagicWithNumbersUnder100(number) {
-        if (number > 0 && number < 20) {
-            return numbersBefore20Arr[number - 1];
+    function makeMagicWithNumbersUnder100(num) {
+        if (num > 0 && num < 20) {
+            return numbersBefore20Arr[num - 1];
         }
-        if (number % 10 === 0) {
-            return doubleDigitsArr[number / 10 - 2];
+        if (num % 10 === 0) {
+            return doubleDigitsArr[num / 10 - 2];
         }
 
-        let funcArr = (number / 10).toString().split(".");
+        let funcArr = (num / 10).toString().split(".");
         return `${doubleDigitsArr[+funcArr[0] - 2]} ${
             numbersBefore20Arr[funcArr[1] - 1]
         }`;
+    }
+
+    if (number < 100) {
+        resultString = makeMagicWithNumbersUnder100(number);
+    }
+
+    if (number >= 100) {
+        let firstNum = Math.floor(number / 100);
+
+        if (number % 100 === 0) {
+            return `${numbersBefore20Arr[firstNum - 1]} hundred`;
+        }
+        let secondsNum = Number(number.toString().split("").splice(1).join(""));
+        console.log(`Seconds ${secondsNum}`);
+        return `${
+            numbersBefore20Arr[firstNum - 1]
+        } hundred ${makeMagicWithNumbersUnder100(secondsNum)}`;
     }
 
     return resultString;
