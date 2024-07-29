8af2d25b7dd44bb1ac998f72d40d23199c497b25
Vadim Polovinko <vadimpolovinko89@gmail.com>
2024-07-30 01:00:47 +0300

    feat: add mane function that makes logic with numbers under 100

--- src/index.js
+++ src/index.js
@@ -33,4 +33,20 @@ module.exports = function toReadable(number) {
     ];
 
     let resultString = "";
+
+    function makeMagicWithNumbersUnder100(number) {
+        if (number > 0 && number < 20) {
+            return numbersBefore20Arr[number - 1];
+        }
+        if (number % 10 === 0) {
+            return doubleDigitsArr[number / 10 - 2];
+        }
+
+        let funcArr = (number / 10).toString().split(".");
+        return `${doubleDigitsArr[+funcArr[0] - 2]} ${
+            numbersBefore20Arr[funcArr[1] - 1]
+        }`;
+    }
+
+    return resultString;
 };
