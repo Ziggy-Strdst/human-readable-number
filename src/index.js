092a8f391080fbe5f26d5b96c7f219e2b5740e33
Vadim Polovinko <vadimpolovinko89@gmail.com>
2024-07-30 00:51:28 +0300

    feat: add arrays with words equals numbers

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 module.exports = function toReadable(number) {
-    //
+    const numbersBefore20Arr = [
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+        "ten",
+        "eleven",
+        "twelve",
+        "thirteen",
+        "fourteen",
+        "fifteen",
+        "sixteen",
+        "seventeen",
+        "eighteen",
+        "nineteen",
+    ];
+
+    const doubleDigitsArr = [
+        "twenty",
+        "thirty",
+        "fourty",
+        "fifty",
+        "sixty",
+        "seventy",
+        "eighty",
+        "ninety",
+    ];
+
+    let resultString = "";
+    ``;
 };
